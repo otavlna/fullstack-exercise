e7ab4ced3ac758950e23074cf32a1971f1fd0c5b
Ota Vlna <otavlna@protonmail.ch>
2022-07-15 19:21:32 +0200

    feat: serve static images

--- fullstack-exercise-backend/src/main.ts
+++ fullstack-exercise-backend/src/main.ts
@@ -1,14 +1,20 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
+import { cwd } from 'process';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
       transform: true,
     }),
+  );
+  app.useStaticAssets(
+    join(cwd(), process.env.UPLOADED_FILES_DEST ?? 'uploads'),
   );
   await app.listen(1337);
 }
