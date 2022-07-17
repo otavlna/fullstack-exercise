24be0e2fbb3ddeea05d5667959002b2b77e4a3ef
Ota Vlna <otavlna@protonmail.ch>
2022-07-17 10:52:01 +0200

    fix: :bug: correct id property that matches UserEntity

--- fullstack-exercise-backend/src/modules/auth/auth.service.ts
+++ fullstack-exercise-backend/src/modules/auth/auth.service.ts
@@ -39,7 +39,7 @@ export class AuthService {
   }
 
   async login(user: any) {
-    const payload = { username: user.username, sub: user.userId };
+    const payload = { id: user.id, username: user.username };
     return {
       access_token: this.jwtService.sign(payload),
     };
