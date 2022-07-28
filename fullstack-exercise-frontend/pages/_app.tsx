68cdf8aa9fcdca9d5cbf39d65e47427909a5a4a6
Ota Vlna <otavlna@protonmail.ch>
2022-07-28 18:14:56 +0200

    feat: setup app layout

--- fullstack-exercise-frontend/pages/_app.tsx
+++ fullstack-exercise-frontend/pages/_app.tsx
@@ -1,11 +1,15 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "../styles/app.scss";
 import Layout from "../components/layout";
 import type { AppProps } from "next/app";
+import { Provider } from "react-redux";
+import store from "../store/store";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   );
 }
