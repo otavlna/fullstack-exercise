d0f1b5941b13cc7d324d689254415740f557a580
Ota Vlna <otavlna@protonmail.ch>
2022-07-31 13:02:28 +0200

    fix: make images the same width

--- fullstack-exercise-frontend/features/articles/articlePreview.tsx
+++ fullstack-exercise-frontend/features/articles/articlePreview.tsx
@@ -10,30 +10,34 @@ type ArticlePreviewProps = {
 
 const ArticlePreview: FunctionComponent<ArticlePreviewProps> = ({ article }) => {
   return (
-    <article className="d-flex flex-direction-row mb-3">
-      <div className="image-preview-container">
-        <Image
-          layout="fill"
-          objectFit="cover"
-          src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${article.fileName}`}
-          alt="Article image"
-        ></Image>
-      </div>
-      <div className="ms-3">
-        <h4>{article.title}</h4>
-        <div>
-          <small className="text-secondary">{article.user.username}</small>
-          <span className="mx-2 text-secondary">•</span>
-          <small className="text-secondary ">{formatDate(article.createdAt)}</small>
-          <p className="mt-2">{article.perex}</p>
-          <div className="ms-1">
-            <Link href={`article/${article.id}`}>
-              <a>
-                <small>Read whole article</small>
-              </a>
-            </Link>
-            <small className="ms-2 text-secondary">{article.comments} comments</small>
+    <article className="mb-3 row">
+      <div className="col-12 col-md-8">
+        <div className="row g-4">
+        <div className="image-preview-container col-4">
+          <Image
+            layout="fill"
+            objectFit="cover"
+            src={`${process.env.NEXT_PUBLIC_API_BASE_URL}/${article.fileName}`}
+            alt="Article image"
+          ></Image>
+        </div>
+        <div className="col-8">
+          <h4>{article.title}</h4>
+          <div>
+            <small className="text-secondary">{article.user.username}</small>
+            <span className="mx-2 text-secondary">•</span>
+            <small className="text-secondary ">{formatDate(article.createdAt)}</small>
+            <p className="mt-2">{article.perex}</p>
+            <div className="ms-1">
+              <Link href={`article/${article.id}`}>
+                <a>
+                  <small>Read whole article</small>
+                </a>
+              </Link>
+              <small className="ms-2 text-secondary">{article.comments} comments</small>
+            </div>
           </div>
+        </div>
         </div>
       </div>
     </article>
